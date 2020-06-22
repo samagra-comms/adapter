1fc3ff09417a7052e587700c070c8a5a5e684d8d
Vishal.Singla <vsingla160@gmail.com>
2020-06-22 11:31:47 +0530

    refactored code with new arch.

---
+++ src/main/java/com/samagra/adapter/app/config/AppConfiguration1.java
@@ -0,0 +1,45 @@
+package com.samagra.adapter.app.config;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableAutoConfiguration
+public class AppConfiguration1 {
+
+    @Bean
+    @Qualifier("rest")
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @Qualifier("custom")
+    public RestTemplate getCustomTemplate(RestTemplateBuilder builder) {
+        Credentials credentials = new UsernamePasswordCredentials("samagra","impact@scale");
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, credentials);
+
+        HttpClient httpClient = HttpClients
+                .custom()
+                .setDefaultCredentialsProvider(credentialsProvider)
+                .disableAuthCaching()
+                .build();
+
+        return builder
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+                .build();
+    }
+}
--- src/test/java/com/samagra/adapter/gs/whatsapp/GupShupWhatsappAdapterTest.java
+++ src/test/java/com/samagra/adapter/gs/whatsapp/GupShupWhatsappAdapterTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samagra.common.Request.GSWhatsAppMessage;
 import messagerosa.core.model.XMessage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.bind.JAXBException;
 
@@ -12,13 +13,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GupShupWhatsappAdapterTest {
 
+    @Autowired
+    private GupShupWhatsappAdapter adapter;
     @Test
     public void testForSimplePayload() throws JsonProcessingException, JAXBException {
         // Get this from https://www.gupshup.io/developer/docs/bot-platform/guide/whatsapp-api-documentation#IMNE
         // Convert it to string using => https://www.freeformatter.com/json-escape.html#ad-output
         String payload = "{\r\n            \"app\": \"DemoApp\",\r\n                \"timestamp\": 1580227766370,\r\n                \"version\": 2,\r\n                \"type\": \"message\",\r\n                \"payload\": {\r\n            \"id\": \"ABEGkYaYVSEEAhAL3SLAWwHKeKrt6s3FKB0c\",\r\n                    \"source\": \"918x98xx21x4\",\r\n                    \"type\": \"text\",\r\n                    \"payload\": {\r\n                \"text\": \"Hi\"\r\n            },\r\n            \"sender\": {\r\n                \"phone\": \"918x98xx21x4\",\r\n                        \"name\": \"Smit\",\r\n                        \"country_code\": \"91\",\r\n                        \"dial_code\": \"8x98xx21x4\"\r\n            }\r\n        }\r\n        }";
         GSWhatsAppMessage message = new ObjectMapper().readValue(payload, GSWhatsAppMessage.class);
-        XMessage xMessage = GupShupWhatsappAdapter.convertMessageToXMsg(message);
+        XMessage xMessage = adapter.convertMessageToXMsg(message);
         assertEquals(xMessage.getMessageId(), "ABEGkYaYVSEEAhAL3SLAWwHKeKrt6s3FKB0c");
     }
 
