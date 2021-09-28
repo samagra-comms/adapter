3d484df0df633e2f9c3c51bec268fd33c010d7ce
Surabhi <surabhi.mahawar@aurigait.com>
2021-09-28 16:17:50 +0530

    changes

--- src/test/java/com/uci/adapter/gs/sms/GupshupSMSAdapterTest.java
+++ src/test/java/com/uci/adapter/gs/sms/GupshupSMSAdapterTest.java
@@ -1,5 +1,7 @@
 package com.uci.adapter.gs.sms;
 
+import java.util.ArrayList;
+
 import javax.xml.bind.JAXBException;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +17,7 @@ import com.uci.adapter.netcore.whatsapp.inbound.NetcoreWhatsAppMessage;
 import com.uci.utils.BotService;
 
 import lombok.SneakyThrows;
+import messagerosa.core.model.ButtonChoice;
 import messagerosa.core.model.SenderReceiverInfo;
 import messagerosa.core.model.XMessage;
 import messagerosa.core.model.XMessagePayload;
@@ -39,7 +42,7 @@ public class GupshupSMSAdapterTest {
 		
 		try {
 			XMessage response = adapter.callOutBoundAPI(xmsg);
-			System.out.println(response);
+//			System.out.println(xmsg.toXML());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,7 +52,15 @@ public class GupshupSMSAdapterTest {
 	/* XMessage object for testing */
     private XMessage getXMessageForTest() {
     	SenderReceiverInfo to = SenderReceiverInfo.builder().userID("917597185708").build();
+//    	ButtonChoice choice = ButtonChoice.builder().key("1").text("1a").build();
+//    	ButtonChoice choice2 = ButtonChoice.builder().key("2").text("2a").build();
+//    	ArrayList list = new ArrayList();
+//    	list.add(choice);
+//    	list.add(choice2);
+    	
+//		XMessagePayload payload = XMessagePayload.builder().text("Hey").buttonChoices(list).build();
 		XMessagePayload payload = XMessagePayload.builder().text("Hey").build();
+
 		return XMessage.builder().to(to).payload(payload).build();
     }
 }	
